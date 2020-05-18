c8ad8b03e2a5a49c2abd1e27dd75c49868679e70
Chu-Te <54582940+Jordon-Chen@users.noreply.github.com>
2020-05-18 22:47:09 +0800

    upload

---
+++ js/AleartPromptConsolelog.js
@@ -0,0 +1,5 @@
+const FirstName = prompt("Please Enter your first name");
+const LastName = prompt("Please Enter your last name");
+const age = prompt("Please Enter your age");
+console.log("Your full name is " + FirstName + " " + LastName +".");
+console.log("Your age is " + age + " " + "years old.");
