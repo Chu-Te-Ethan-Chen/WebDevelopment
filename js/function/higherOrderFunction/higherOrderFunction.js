ba48668cae02101d37f1f1502db39d3fcf32880f
Chu-Te <54582940+Jordon-Chen@users.noreply.github.com>
2020-05-29 23:21:01 +0800

    update

---
+++ js/function/higherOrderFunction/higherOrderFunction.js
@@ -0,0 +1,12 @@
+// function sing(){
+//     console.log("twinkle winkle ...");
+//     console.log("how i wonder ...");
+// }
+// setInterval(sing, 1000);
+
+
+// anonymous function
+setInterval(function(){
+    console.log("I'm an anonymous function!");
+    console.log("THIS IS AWESOME!!!")
+}, 2000);
